898330c9084dc609429bbbe44da61a74fee038ff
jonathanhenriques <silva.henriquejonas@gmail.com>
2022-05-14 15:15:16 -0300

    Testando reset pull

--- LojaRPG/src/LojaRPG/Lojas/LojaGeral.java
+++ LojaRPG/src/LojaRPG/Lojas/LojaGeral.java
@@ -3,59 +3,49 @@ package LojaRPG.Lojas;
 import LojaRPG.Item;
 import LojaRPG.Loja;
 
-public class LojaGeral implements Loja
-{
-
+public class LojaGeral implements Loja {
+	String teste = "TESTANDO FRASE NO PROJETO";
 	Item item = new Item();
-	
+
 	private double caixaLoja;// DINHEIRO NO CAIXA
 	private int espacoLoja;// TAMANHO A DEFINIR DA LOJA
 	private int metroQuadro[];// METRO QUADRADO DA LOJA
-	private Item produto[];//PRODUTOS DIVERSOS
-	private double especimeCobre;//A MOEDA DO LOCAL
-	
-	//METODO CONSTRUTOR
-	public LojaGeral()
-	{
+	private Item produto[];// PRODUTOS DIVERSOS
+	private double especimeCobre;// A MOEDA DO LOCAL
+
+	// METODO CONSTRUTOR
+	public LojaGeral() {
 		this.caixaLoja = 2000.50;
 	}
 
 	@Override
-	public void comprar()
-	{
+	public void comprar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void vender()
-	{
+	public void vender() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void adicionar()
-	{
+	public void adicionar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void remover()
-	{
+	public void remover() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void catalogoLoja()
-	{
+	public void catalogoLoja() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
-	
-	
+
 }
