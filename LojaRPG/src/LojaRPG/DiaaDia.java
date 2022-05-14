98ec2b268d9de4197fbe830d62ccbd28b0c7c065
Renan <baratellarenan@gmail.com>
2022-05-14 20:27:26 -0300

    Alterado somente classe funcionario e o main

--- LojaRPG/src/LojaRPG/DiaaDia.java
+++ LojaRPG/src/LojaRPG/DiaaDia.java
@@ -1,5 +1,4 @@
 package LojaRPG;
-
 import LojaRPG.Pessoas.Funcionario;
 
 public class DiaaDia
@@ -7,34 +6,17 @@ public class DiaaDia
 
 	public static void main(String[] args)
 	{
-		Funcionario funcionario = new Funcionario();
-		funcionario.setNome("Joao");
-		funcionario.setRaca("Elfo");
-		funcionario.setCargo("Vendedor");
-		funcionario.definirSalarioComCargo(funcionario.getCargo());
 		
-		funcionario.status();
 		
-		System.out.println();
 		
-		Funcionario funcionario2 = new Funcionario();
-		funcionario2.setNome("Joaorggggf");
-		funcionario2.setRaca("Elfofgfgfggf");
-		funcionario2.setCargo("Vendedor");
-		funcionario2.definirSalarioComCargo(funcionario2.getCargo());
-		
-		funcionario2.status();
-		
-		System.out.println();
-		
-		Funcionario funcionario3 = new Funcionario();
-		funcionario3.setNome("Joaorggggf");
-		funcionario3.setRaca("Elfofgfgfggf");
-		funcionario3.setCargo("Gerente");
-		funcionario3.definirSalarioComCargo(funcionario3.getCargo());
-	
-		funcionario3.status();
-		
+		/*
+		int nmr;
+		nmr = Funcionario.menu();        
+		System.out.println(nmr);
+		ir até a linha 87 da classe funcionario e altera o valor da variavel nmr
+
+		*/
+
 
 	}
 
--- LojaRPG/src/LojaRPG/Pessoas/Funcionario.java
+++ LojaRPG/src/LojaRPG/Pessoas/Funcionario.java
@@ -1,14 +1,16 @@
 package LojaRPG.Pessoas;
 
-public class Funcionario extends Pessoa
-{
-	
-	private int cod;
-	private static int numberInstances;
-	private double salario;//GANHA PÃO
-	private boolean chave = false;//ACESSAR A LOJA
-	private String cargo;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Funcionario extends Pessoa{
+
+	private int cod;
+	private static int numberInstances; // contador de objetos instanciados.
+	private double salario;
+	private String cargo;
+	private boolean disp; // se está disponível para atender o cliente
+	
 	public Funcionario() {
 		numberInstances++;
 		cod = numberInstances;
@@ -24,6 +26,76 @@ public class Funcionario extends Pessoa
 		return cargo;
 	}
 	
+	public static int menu() {
+		
+		ArrayList<Funcionario> funcionarios = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		int nmr=0;
+		
+		Funcionario funcionario1 = new Funcionario();
+		funcionario1.setNome("Joao");
+		funcionario1.setRaca("Elfo");
+		funcionario1.setCargo("Gerente");
+		funcionario1.setDisp(false);
+		funcionario1.definirSalarioComCargo(funcionario1.getCargo());
+		
+		Funcionario funcionario2 = new Funcionario();
+		funcionario2.setNome("Rebecca");
+		funcionario2.setRaca("Demonio");
+		funcionario2.setCargo("Analista");
+		funcionario2.setDisp(true);
+		funcionario2.definirSalarioComCargo(funcionario2.getCargo());
+		
+		Funcionario funcionario3 = new Funcionario();
+		funcionario3.setNome("Tadeu");
+		funcionario3.setRaca("Humano");
+		funcionario3.setCargo("Vendedor");
+		funcionario3.setDisp(true);
+		funcionario3.definirSalarioComCargo(funcionario3.getCargo());
+	
+		funcionarios.add(funcionario1);
+		funcionarios.add(funcionario2);
+		funcionarios.add(funcionario3);
+		
+		do
+		{
+		
+			System.out.println("\nOlá \"nome do cliente\" você deseja ser atendido por qual funcionário? ");
+			System.out.println("Temos os seguintes funcionários disponíveis: \n");
+			
+			for(int i = 0; i<funcionarios.size();i++) {
+				if(funcionarios.get(i).isDisp())
+					System.out.println(i+1+" - " + funcionarios.get(i));
+			}
+		
+			System.out.println();
+			System.out.println(funcionarios.size()+1+" - Para ver maiores informações sobre os funcionários.");
+			nmr = sc.nextInt();
+			
+			if(nmr==funcionarios.size()+1) {
+				System.out.println("\nLista de todos os Funcionários na nossa Loja: \n");
+				for(Funcionario func : funcionarios) {
+					System.out.println(func.status());
+				}
+			}
+		
+			System.out.println("1 - para voltar a tela anterior\n0 - para sair do programa");
+			nmr = sc.nextInt();
+	
+		}while(nmr != 0);
+		sc.close();
+		nmr = 0;
+		return nmr;
+	}
+	
+	public boolean isDisp() {
+		return disp;
+	}
+
+	public void setDisp(boolean disp) {
+		this.disp = disp;
+	}
+
 	public int getCod() { 
 		return cod;
 	}
@@ -36,14 +108,6 @@ public class Funcionario extends Pessoa
 		this.salario = salario;
 	}
 	
-	public boolean isChave() {
-		return chave;
-	}
-	
-	public void setChave(boolean chave) {
-		this.chave = chave;
-	}
-	
 	public String getCargo() {
 		return cargo;
 	}
@@ -52,12 +116,13 @@ public class Funcionario extends Pessoa
 		this.cargo = cargo;
 	}
 	
-	public void entrarLoja() {
-		this.chave = true;
+	public String status() {
+		return "Nome: "+getNome()+", Raça: "+getRaca()+", Cargo: "+getCargo()+", Salário: "+getSalario();
 	}
 	
-	public void status() {
-		System.out.println(getCod()+"\n"+getNome()+"\n"+getRaca()+"\n"+getSalario()+"\n"+getCargo());
+	public String toString() {
+		return "Nome: "+getNome()+", Raça: "+getRaca()+", Cargo: "+getCargo();
+		
 	}
 	
 }
