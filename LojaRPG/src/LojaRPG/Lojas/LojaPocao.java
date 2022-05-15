c853cad330190749c28dd746af297c0cbbfea0d7
Edgard Henrique <edigarhenriqu@gmail.com>
2022-05-15 12:27:28 -0300

    Loja Atualizada

--- LojaRPG/src/LojaRPG/Lojas/LojaPocao.java
+++ LojaRPG/src/LojaRPG/Lojas/LojaPocao.java
@@ -1,6 +1,6 @@
 package LojaRPG.Lojas;
 
-public class LojaPocao extends LojaGeral
+public abstract class LojaPocao extends LojaGeral
 {
 	private int tipo;
 	private String nome;
@@ -8,5 +8,63 @@ public class LojaPocao extends LojaGeral
 	private double valorUnitario;
 	
 	public LojaPocao(int tipo, String nome) {
+	
+		this.tipo = tipo;
+		
+	}
 
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	
+	public void incluirEstoque(int quantidade) {
+		estoque = estoque + quantidade;
+		
+	}
+	
+	public void tirarEstoque(int quantidade) {
+		
+		if(quantidade <= estoque) {
+			
+			estoque = estoque - quantidade;
+			
+		}
+		else 
+		{
+			System.out.println("Estoque indisponivel!!!");
+		
+		}
+	}
+	
 }
+	
+
+	
